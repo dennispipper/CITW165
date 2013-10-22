057c66964af5eb1f9ea0cebbf351e3349f81a2a9
spartyguy <dennis@pipper.net>
2013-10-22 15:40:42 -0400

    added correct faq.js file
    The incorrect file was transferred

--- exercises/ch09/faqs/faqs.js
+++ exercises/ch09/faqs/faqs.js
@@ -3,14 +3,8 @@ $(document).ready(function() {
 	$("#faqs div").first().show();
 	$("#faqs h2").click(
 		function() {
-			if (! $(this).hasClass("minus")) {
-				$("h2").filter(".minus").next().slideUp(1000);
-				$("h2").filter(".minus").removeClass("minus");
-				$(this).addClass("minus");
-		    	$(this).next().slideDown(1000);
-		    	// $(this).toggleClass("minus");
-		    	// $(this).next().slideToggle(1000);								
-			}
-	    }
+			$(this).toggleClass("minus");
+		    $(this).next().slideToggle(1000);								
+		}
     ); // end click
 }); // end ready
